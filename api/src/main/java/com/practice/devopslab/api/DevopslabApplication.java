ab9d9ea2c4db3ceba778d10206752cab4531377c
SeungGyeong <tpsseung@gmail.com>
2025-05-25 20:04:40 +0900

    initial commit

---
+++ api/src/main/java/com/practice/devopslab/api/DevopslabApplication.java
@@ -0,0 +1,13 @@
+package com.practice.devopslab.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopslabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopslabApplication.class, args);
+	}
+
+}
